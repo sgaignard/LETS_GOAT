262e513098126255bcaa0f4e36d4c5a746c3cc68
Renaud BORLET <renaud_borlet@hotmail.com>
2022-05-24 12:56:00 +0200

    goats lady goats

--- app/models/goat.rb
+++ app/models/goat.rb
@@ -1,4 +1,5 @@
 class Goat < ApplicationRecord
   has_many :bookings
   has_many :users, through: :bookings
+  belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,46 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+# require 'open-uri'
+
+# url = "https://tmdb.lewagon.com/movie/top_rated"
+# films_db = URI.open(url).read
+# films = JSON.parse(films_db)
+# films["results"].each { |element|
+#   Movie.create(title: element["title"], overview: element["overview"], poster_url: "https://tmdb.lewagon.com/movie/top_rated#{element["poster_path"]}", rating: element["vote_average"])
+# }
+require "faker"
+
+Goat.destroy_all
+User.destroy_all
+
+10.times do
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+    )
+  user.save!
+  3.times do
+    goat = Goat.new(
+      gender: Faker::Gender.binary_type,
+      color: Faker::Creature::Cat.breed,
+      cuteness: rand(1..10),
+      speed: rand(5..20),
+    )
+    goat.name = goat.gender == "male" ? (Faker::Name.male_first_name) : (Faker::Name.female_first_name)
+    goat.price = 0.3 * goat.cuteness + (1 / goat.speed) * 3
+    goat.user = user
+    goat.save!
+  end
+end
+
+p User.all
+p User.count
+p Goat.all
+p Goat.count
