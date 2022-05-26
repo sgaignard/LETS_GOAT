2412f5581552f58bb0c58981ae384eeb5c266b24
Fanny Farrouch <fanny.farrouch@gmail.com>
2022-05-26 11:48:40 +0200

    Form refacto & selective option

--- app/models/goat.rb
+++ app/models/goat.rb
@@ -3,4 +3,12 @@ class Goat < ApplicationRecord
   has_many :users, through: :bookings
   belongs_to :user
   has_one_attached :photo
+
+  validates :name, presence: true
+  validates :color, presence: true
+  validates :gender, presence: true
+  # validates :cuteness
+  validates :price, presence: true
+  validates :speed, presence: true
+  validates :description, presence: true
 end
